6e874c60bdd69da768e375c44feff0ddede6108a
gaona02 <gaona02@baidu.com>
2018-02-28 12:11:19 +0800

    redux-study

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+    devtool: 'cheap-module-eval-source-map',
+    entry: [
+        'webpack-hot-middleware/client',
+        './index'
+    ],
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: 'bundle.js',
+        publicPath: '/static'
+    },
+    plugins: [
+        new webpack.optimize.OccurrenceOrderPlugin(),
+        new webpack.HotModuleReplacementPlugin()
+    ],
+    module: {
+        loaders: [
+            { 
+                test: /\.js|jsx$/, 
+                //loaders: [ 'babel-loader', 'env', 'react' ],
+                loader: 'babel-loader',
+                query: {
+                    presets: ['env', 'react']
+                },
+                exclude: /node_modules/, 
+                include: __dirname
+            }
+        ]
+    }
+}
